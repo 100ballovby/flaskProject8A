b237ed15e1427bd32bfb5ad0611b9a1bd32adf05
demidraksin <greatraksin@icloud.com>
2021-01-15 13:46:49 +0300

    Editing routing, adding new view function

--- routes.py
+++ routes.py
@@ -1,2 +1,18 @@
-''' маршрутизация сайта'''
+''' маршрутизация сайта '''
+from app import app  # импортирую переменную сайта
+from flask import render_template  # рисует страницу
 
+
+@app.route('/')
+def main_page():
+    return render_template('index.html', title='Главная страница!')
+
+
+@app.route('/contacts')
+def contact_page():
+    return render_template('contacts.html')
+
+
+@app.route('/gallery')
+def gallery_page():
+    return render_template('gallery.html')
